b004d1f3cc6d8fb3b17069e96ce6ed240290d5c3
David Cao <58381393+davidcao1337@users.noreply.github.com>
2020-03-28 16:35:16 -0400

    Update main.py
    Alpha 2.0 update

--- main.py
+++ main.py
@@ -3,25 +3,32 @@ from classes import Player
 import functions
 
 # Title / Intro
-print("----------Chip Trade----------")
-print("Rules are included in separate document\n")
+functions.display_intro()
 
 # Initialize player
 thePlayer = Player()
 
+# Turn tracker
+turn_count = 1
+
 # Prompt player for action
 while (thePlayer.chip_amt > 0) and (thePlayer.card_amt != 8):
+    print("~[Turn", str(turn_count) + "]~")
     functions.display_stats(thePlayer)
     print("Select an action: ")
-    if thePlayer.grave_yrd_amt < 2:
-        action = input("1.) Roll\t2.) Libra's Scale\nSelection:")
+    if thePlayer.protected_rolls > 0:
+        # If Paladin's Shield is active, the player can only roll
+        action = input("1.) Roll\nSelection: ")
+    elif thePlayer.grave_yrd_amt < 2:
+        action = input("1.) Roll\t2.) Libra's Scale\nSelection: ")
     else:
-        action = input("1.) Roll\t2.) Libra's Scale\t3.) Paladin's Shield\t4.)Green Skull\t5.) Red Bandage\nSelection:")
+        action = input("1.) Roll\t2.) Libra's Scale\t3.) Paladin's Shield\t4.)Green Skull\t5.) Red Bandage\nSelection: ")
 
     # Action Cases
 
     # Roll Action (Basic Mechanic)
     if action == "1":
+        print("\n-----Roll Result-----")
         faced_up = 0
         for chip in range(thePlayer.chip_amt):
             # Flip a chip
@@ -29,14 +36,14 @@ while (thePlayer.chip_amt > 0) and (thePlayer.card_amt != 8):
             # 0 = Face Down; 1 = Face Up
             if flip_result == 1:
                 faced_up += 1
-        print(faced_up, "are faced up!")
+        print(faced_up, "faced up!")
 
         # All 7 faced up or down case:
         if(faced_up == 7) or ((faced_up == 0) and (thePlayer.chip_amt == 7)):
             print("Lucky 7!")
             thePlayer.card_amt += 3
         # All 6 or less chips faced up case:
-        elif faced_up == thePlayer.chip_amt:
+        elif (faced_up == thePlayer.chip_amt) and (thePlayer.protected_rolls == 0) and (thePlayer.chip_amt != 1):
             print("All chips faced up! +1 Card and +1 Power-Up of your choice!")
             thePlayer.card_amt += 1
             print("Select a Power-Up that you want:")
@@ -51,7 +58,7 @@ while (thePlayer.chip_amt > 0) and (thePlayer.card_amt != 8):
             elif desPwr == "4":
                 thePlayer.bandage_use += 1
         # All 6 or less chips faced down case:
-        elif faced_up == 0:
+        elif (faced_up == 0) and (thePlayer.protected_rolls == 0) and (thePlayer.chip_amt != 1):
             print("All chips faced down! +1 Chip")
             thePlayer.grave_yrd_amt -= 1
             thePlayer.chip_amt += 1
@@ -81,9 +88,12 @@ while (thePlayer.chip_amt > 0) and (thePlayer.card_amt != 8):
             thePlayer.protected_rolls -= 1
             if thePlayer.protected_rolls == 0:
                 print("The Paladin's Shield no longer guards you...")
+        print()
+        turn_count += 1
 
     # Libra's Scale
-    elif action == "2":
+    elif (action == "2") and (thePlayer.scale_use > 0) and (thePlayer.protected_rolls == 0):
+        print("\n-----Power Result-----")
         if thePlayer.scale_use > 0:
             thePlayer.scale_use -= 1
             # Rolls for amount of chips lost
@@ -95,16 +105,16 @@ while (thePlayer.chip_amt > 0) and (thePlayer.card_amt != 8):
             second_roll = random.randint(1, 4)
             print("The second weight is", second_roll, "\n+" + str(second_roll), "Cards")
             thePlayer.card_amt += second_roll
-        else:
-            print("There's no use of that power left!")
-            # Loop action prompt
+        print()
+        turn_count += 1
 
     # Paladin's Shield
-    elif action == "3":
+    elif (action == "3") and (thePlayer.pal_use > 0) and (thePlayer.grave_yrd_amt >= 2) and (thePlayer.protected_rolls == 0):
         thePlayer.pal_use -= 1
         print("What power would you like to give up (1 use):")
         print("1.) Libra's Scale\t2.) Green Skull\t3.) Red Bandage")
         pwr_give_up = input("Selection: ")
+        print("\n-----Power Result-----")
         if pwr_give_up == "1":
             thePlayer.scale_use -= 1
         elif pwr_give_up == "2":
@@ -119,9 +129,12 @@ while (thePlayer.chip_amt > 0) and (thePlayer.card_amt != 8):
             # Paladin's Shield success
             print("The Paladin's Shield guards you!")
             thePlayer.protected_rolls = (8 - thePlayer.card_amt)
+        print()
+        turn_count += 1
 
     # Green Skull
-    elif action == "4":
+    elif (action == "4") and (thePlayer.skull_use > 0) and (thePlayer.grave_yrd_amt >= 2) and (thePlayer.protected_rolls == 0):
+        print("\n-----Power Result-----")
         thePlayer.skull_use -= 1
         skull_result = random.randint(0, 1)
         if skull_result == 0:
@@ -133,9 +146,12 @@ while (thePlayer.chip_amt > 0) and (thePlayer.card_amt != 8):
             thePlayer.card_amt += thePlayer.grave_yrd_amt
             thePlayer.chip_amt = 7
             thePlayer.grave_yrd_amt = 0
+        print()
+        turn_count += 1
 
     # Red Bandage
-    elif action == "5":
+    elif (action == "5") and (thePlayer.bandage_use > 0) and (thePlayer.grave_yrd_amt >= 2) and (thePlayer.protected_rolls == 0):
+        print("\n-----Power Result-----")
         thePlayer.bandage_use -= 1
         bandage_result = random.randint(0, 1)
         if bandage_result == 0:
@@ -159,14 +175,34 @@ while (thePlayer.chip_amt > 0) and (thePlayer.card_amt != 8):
                 # 0 = Face Down; 1 = Face Up
                 if flip_result == 1:
                     faced_up += 1
-            print("You have recovered! +" + str(faced_up), "Chips")
-            thePlayer.chip_amt += faced_up
-            thePlayer.grave_yrd_amt -= faced_up
+            # Calculates the final amount of chips the player will have after recovery
+            total_chips = thePlayer.chip_amt + faced_up
+            # If it results in an over-heal (or full-heal), fully restores player to max of 7 chips
+            if total_chips >= 7:
+                print("You have fully recovered!")
+                thePlayer.chip_amt = 7
+                thePlayer.grave_yrd_amt = 0
+            # Unlucky case where the bandage succeeds, but ZERO chips are FACED UP in the roll...
+            elif faced_up == 0:
+                print("The bandage's power was too weak!")
+                print("You have recovered! +" + str(faced_up), "Chips")
+            # If not an over-heal/full-heal, normal rule applies
+            else:
+                print("You have recovered! +" + str(faced_up), "Chips")
+                thePlayer.chip_amt = total_chips
+                thePlayer.grave_yrd_amt -= faced_up
             print("You have been rewarded! +" + str(faced_up), "Cards")
             thePlayer.card_amt += faced_up
+        print()
+        turn_count += 1
 
-functions.display_stats(thePlayer)
-if thePlayer.card_amt == 8:
-    print("-----VICTORY-----")
-else:
-    print("-----DEFEAT-----")
+    # Invalid Action
+    else:
+        print("Invalid selection...\n")
+        # When player attempts to use a power that has no uses left
+        functions.insufficient_pwr_msg(action, thePlayer)
+        functions.pal_active_msg(action, thePlayer)
+
+
+# Display Game Result
+functions.display_game_result(thePlayer)
